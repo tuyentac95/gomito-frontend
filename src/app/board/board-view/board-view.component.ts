60caeed0a4f49862648380a7f31d12317ae3147a
tuyentac95 <tuyentac95@gmail.com>
2020-11-04 00:08:18 +0700

    desgin giao diện kéo thả card

---
+++ src/app/board/board-view/board-view.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-board-view',
+  templateUrl: './board-view.component.html',
+  styleUrls: ['./board-view.component.css']
+})
+export class BoardViewComponent implements OnInit {
+
+  todo = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep'
+  ];
+
+  done = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
+}
