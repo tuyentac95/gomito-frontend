df7f6b6bdf0fe98222c6b53ac5eb3a2e70a37bb3
tuyentac95 <tuyentac95@gmail.com>
2020-11-04 16:03:59 +0700

    thêm form tạo mới board

---
+++ src/app/shared/board-create/board-create.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
+import {BoardModel} from './board-model';
+import {BoardService} from '../../board/board.service';
+
+@Component({
+  selector: 'app-board-create',
+  templateUrl: './board-create.component.html',
+  styleUrls: ['./board-create.component.css']
+})
+export class BoardCreateComponent implements OnInit {
+  boardName: string;
+  newBoard: BoardModel;
+
+  constructor(public dialogRef: MatDialogRef<BoardCreateComponent>,
+              private boardService: BoardService) { }
+
+  ngOnInit(): void {
+    this.boardName = '';
+    this.newBoard = new BoardModel();
+  }
+
+  // tslint:disable-next-line:typedef
+  createBoard() {
+    this.newBoard.name = this.boardName;
+    this.boardService.createBoard(this.newBoard);
+    this.dialogRef.close();
+  }
+}
