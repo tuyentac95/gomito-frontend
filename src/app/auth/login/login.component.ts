88706e118418810a3d43eb33502cab98acd870e2
Nqvinh04 <nqvinh1995@gmail.com>
2020-11-07 11:42:45 +0700

    update validate login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,6 +4,7 @@ import {Router} from '@angular/router';
 import {AuthService} from '../auth.service';
 import {LocalStorageService} from 'ngx-webstorage';
 import {throwError} from 'rxjs';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,27 +13,49 @@ import {throwError} from 'rxjs';
 })
 export class LoginComponent implements OnInit {
 
-  loginRequest: LoginRequest = {
+    loginRequest: LoginRequest = {
     username: '',
     password: ''
   };
+    loginForm: FormGroup;
+  message: string;
 
-  constructor(private authService: AuthService,
-              private router: Router,
-              private localStorage: LocalStorageService) {
+
+  constructor( private fb: FormBuilder,
+               private authService: AuthService,
+               private router: Router,
+               private localStorage: LocalStorageService) {
   }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
   }
 
   login(): void {
+    this.loginRequest.username = this.loginForm.get('username').value;
+    this.loginRequest.password = this.loginForm.get('password').value;
     this.authService.login(this.loginRequest).subscribe((data) => {
-      this.router.navigateByUrl('/dashboard');
-      this.localStorage.store('authenticationToken', data.authenticationToken);
-      this.localStorage.store('username', data.username);
-      this.localStorage.store('userId', data.userId);
-      this.localStorage.store('refreshToken', data.refreshToken);
-      this.localStorage.store('expiresAt', data.expiresAt);
+      if (data.status === 200){
+        this.router.navigateByUrl('/dashboard');
+        this.localStorage.store('authenticationToken', data.authenticationToken);
+        this.localStorage.store('username', data.username);
+        this.localStorage.store('userId', data.userId);
+        this.localStorage.store('refreshToken', data.refreshToken);
+        this.localStorage.store('expiresAt', data.expiresAt);
+      } else if (data.status === 301) {
+        this.message = 'Tài khoản không đúng!';
+        this.router.navigate(['login']);
+      } else if (data.status === 302) {
+        this.message = 'Mật khẩu không đúng!';
+        this.router.navigate(['login']);
+      } else if (data.status === 303) {
+        this.message = 'Tài khoản hoặc mất khẩu không đúng!';
+        this.router.navigate(['login']);
+      }
+
     }, error => {
       throwError(error);
     });
