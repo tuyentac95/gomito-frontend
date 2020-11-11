f327304a39f556ab2dd2b86b7c8e846aa9b69caa
Nqvinh04 <nqvinh1995@gmail.com>
2020-11-11 17:24:15 +0700

    Cập nhật service

---
+++ src/app/comment/comment.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Comment} from '../comment';
+import {environment} from '../../environments/environment';
+
+const API_URL = `${environment.apiUrl}`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllComment(): Observable<Comment[]> {
+      return this.httpClient.get<Comment[]>(API_URL + `/comments`);
+  }
+
+  createComment(comment: Comment): Observable<Comment> {
+    return this.httpClient.post<Comment>(API_URL + '/comments', comment);
+  }
+
+  updateComment(id: number, comment: Comment): Observable<Comment> {
+    return this.httpClient.put<Comment>(API_URL + `/comments/${id}`, comment);
+  }
+
+  deleteComment(id: number): Observable<Comment> {
+    return this.httpClient.delete<Comment>(API_URL + `/comments/${id}`);
+
+  }
+}
